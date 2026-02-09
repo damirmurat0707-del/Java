47145252f2a4468b5392f2af24b05fc58c21fa33
Damir <damirmurat0707@gmail.com>
2026-02-09 12:51:32 +0500

    Lab4

---
+++ .idea/lab4/Student.java
@@ -0,0 +1,29 @@
+public class Student {
+
+    // Метод main — точка входа в Java-программу
+    public static void main(String[] args) {
+
+        // Объявление и инициализация переменной для хранения ФИО студента
+        String fullName = "Damir";
+
+        // Объявление переменной для хранения номера группы
+        String group = "ИС 24-12";
+
+        String age = "18";
+
+        // Объявление переменной для хранения образовательной программы
+        String program = "Информационные системы";
+
+        String  lastName = "Asylbek";
+
+        String clas = "student";
+
+
+        System.out.println("=== Информация о студенте ===");
+        System.out.println("Кластың атауы:" +clas );
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Жасы:" +age );
+        System.out.println("ОП/Специальность: " + program);
+
+    }
+}
